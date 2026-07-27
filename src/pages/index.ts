a36074f6fe50dcd9485faadc6a2df90633191948
theeyad <eyadosman35@gmail.com>
2026-07-27 13:58:11 +0300

    latest refactor

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,9 +2,9 @@ import { lazy } from "react";
 
 export const LoginPage = lazy(() => import("./LoginPage"));
 export const OverviewPage = lazy(() => import("./OverviewPage"));
+export const AnalyticsPage = lazy(() => import("./AnalyticsPage"));
 export const TasksPage = lazy(() => import("./TasksPage"));
 export const TeamPage = lazy(() => import("./TeamPage"));
-export const AnalyticsPage = lazy(() => import("./AnalyticsPage"));
-export const SettingsPage = lazy(() => import("./SettingsPage"));
 export const AccountPage = lazy(() => import("./AccountPage"));
+export const SettingsPage = lazy(() => import("./SettingsPage"));
 export const NotFoundPage = lazy(() => import("./NotFoundPage"));
